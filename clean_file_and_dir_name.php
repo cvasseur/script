de45819b5b420b8f53fe1af8bf45f9091e48a05d
Clément Vasseur <cle.vasseur@gmail.com>
2013-06-30 09:57:13 +0200

    php-cs-fixer

--- clean_file_and_dir_name.php
+++ clean_file_and_dir_name.php
@@ -8,20 +8,19 @@ $strTo = "_";
 $files = scandir($dir);
 foreach ($files as $file) {
   if (strpos($file, $strFrom) !== false) {
-		$path = $dir.DIRECTORY_SEPARATOR.$file;
-		$newPath = $dir.DIRECTORY_SEPARATOR.str_replace($strFrom, $strTo, $file);
+        $path = $dir.DIRECTORY_SEPARATOR.$file;
+        $newPath = $dir.DIRECTORY_SEPARATOR.str_replace($strFrom, $strTo, $file);
 
-		if (is_dir($newPath) || is_file($newPath)) {
-			echo "Unable to rename $path, $newpath already in use\n";
-		  continue;
-		}
+        if (is_dir($newPath) || is_file($newPath)) {
+            echo "Unable to rename $path, $newpath already in use\n";
+          continue;
+        }
 
-		if(rename($path, $newPath)) {
-			echo "Move $path to $newPath\n";
-		} else {
-			echo "Unable to move $path to $newPath\n";
-		}
+        if (rename($path, $newPath)) {
+            echo "Move $path to $newPath\n";
+        } else {
+            echo "Unable to move $path to $newPath\n";
+        }
 
-	}
+    }
 }
-
